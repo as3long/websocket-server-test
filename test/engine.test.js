3781d57575a868f24e192f82ec5a014943bc968c
lonnyhuang(黄龙) <lonnyhuang@tencent.com>
2016-10-26 11:12:02 +0800

    删掉不太需要的文件

--- test/engine.test.js
+++
@@ -1,9 +0,0 @@
-var eio = require('engine.io-client');
-
-var socket = new eio.Socket('ws://localhost:22280/');
-socket.on('open', function(){
-    socket.on('message', function(data){
-        console.log(data);
-    });
-    socket.on('close', function(){});
-});
